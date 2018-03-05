8c5307d389ff6cfc83ac786fc62cb9537fd3fc8b
Ben Teegarden <ben@helloworlddevs.com>
2018-03-04 20:08:05 -0800

    Add logo for mobile, desktop and sticky desktop.

--- web/themes/custom/teeprop/js/global.js
+++ web/themes/custom/teeprop/js/global.js
@@ -21,24 +21,24 @@
    */
   Drupal.behaviors.menuToggle = {
     attach: function(context, settings) {
-      var dropdownEl = $('header.header');
+      var $dropdownEl = $('header.header');
       var openClass = 'menu-open';
       $('.header__menu-toggle').on('click tap touch', function() {
         // Close.
-        if (dropdownEl.hasClass(openClass)) {
-          dropdownEl.removeClass(openClass);
+        if ($dropdownEl.hasClass(openClass)) {
+          $dropdownEl.removeClass(openClass);
         }
         // Open.
         else {
-          dropdownEl.addClass(openClass);
-          dropdownEl.mouseleave(function() {
+          $dropdownEl.addClass(openClass);
+          $dropdownEl.mouseleave(function() {
 
             $(document).click(function(e) {
               e.stopPropagation();
 
               // Check if the clicked area is dropdown or not.
-              if (dropdownEl.has(e.target).length === 0) {
-                dropdownEl.removeClass(openClass);
+              if ($dropdownEl.has(e.target).length === 0) {
+                $dropdownEl.removeClass(openClass);
               }
             })
           });
@@ -47,7 +47,29 @@
 
       // Close menu when menu item is clicked.
       $('.menu--main .menu-item a').on('click tap touch', function() {
-        dropdownEl.removeClass(openClass);
+        $dropdownEl.removeClass(openClass);
+      });
+    }
+  }
+
+  /**
+   * Add class to body when header becomes sticky.
+   */
+  Drupal.behaviors.headerStickyClass = {
+    attach: function(context, settings) {
+      var headerStickyClass = 'header-sticky';
+      var $bodyEl = $('body');
+      var $headerEl = $('header.header');
+      var logoHeight = $('.logo').outerHeight();
+      $(window).scroll(function() {
+        if ($(window).width() > 680) {
+          if ($(window).scrollTop() > logoHeight) {
+            $bodyEl.addClass(headerStickyClass);
+          }
+          else {
+            $bodyEl.removeClass(headerStickyClass);
+          }
+        }
       });
     }
   }
